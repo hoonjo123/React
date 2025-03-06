7fc2850ebe57922eafbcdd84b60f5f4048b43cc2
hoonjo123 <johoon030@gmail.com>
2025-03-06 14:44:44 +0900

    feat:숫자 +버튼을 누르게 되면 전구도 함께 렌더링 되고 있는 문제 => 컴포넌트화 하여 독립적으로 동작하도록 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,16 @@
 import './App.css';
 import { useState } from 'react';
-
-const Bulb = ({ light }) => {
-  console.log(light);
-  return (
-    <div>
-      {light === 'ON' ? (
-        <h1 style={{ backgroundColor: 'orange' }}>ON</h1>
-      ) : (
-        <h1 style={{ backgroundColor: 'gray' }}>OFF</h1>
-      )}
-    </div>
-  );
-};
+import Bulb from './components/Bulb';
+import Counter from './components/Count';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [light, setLight] = useState('OFF');
-
   return (
     <>
       <div>
-        <Bulb light={light} />
-        <button
-          onClick={() => {
-            setLight(light === 'ON' ? 'OFF' : 'ON');
-          }}
-        >
-          {light === 'ON' ? '끄기' : '켜기'}
-        </button>
+        <Counter />
       </div>
       <div>
-        <h1>{count}</h1>
-        <button
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          +
-        </button>
-
-        <button
-          onClick={() => {
-            setCount(count - 1);
-          }}
-        >
-          -
-        </button>
+        <Bulb />
       </div>
     </>
   );
